42bedd3a6631292f2fc551a616859407c78b1a9e
hujieying <hujieying0729@163.com>
2017-12-21 16:28:50 +0800

    update

--- test/numpyTest.py
+++ test/numpyTest.py
@@ -1,18 +1,118 @@
 # encoding=utf-8
 
 import numpy as np
+from numpy.linalg import *
 
 def main():
-    lst=[[1,3,5],[2,4,6]]
-    print(type(lst))
-    np_list=np.array(lst)
-    print(type(np_list))
-    np_list=np.array(lst,dtype=np.float)
-    print(np_list.shape)
-    print(np_list.ndim)  #维数
-    print(np_list.dtype)
-    print(np_list.itemsize) #每个元素大小
-    print(np_list.size)
+    # lst=[[1,3,5],[2,4,6]]
+    # print(type(lst))
+    # np_list=np.array(lst)
+    # print(type(np_list))
+    # np_list=np.array(lst,dtype=np.float)
+    # print(np_list.shape)
+    # print(np_list.ndim)  #维数
+    # print(np_list.dtype)
+    # print(np_list.itemsize) #每个元素大小
+    # print(np_list.size)
+    #
+    # #2 some arrays
+    # print(np.zeros([2,4])) #0数组,进行数值初始化
+    # print(np.ones([3,5]))
+    # print("Rand:")
+    # print(np.random.rand(2,4))
+    # print(np.random.rand())
+    # print("RandInt:")
+    # print(np.random.randint(1,10,3))
+    # print("Randn:")
+    # print(np.random.randn(2,4))
+    # print("Choice:")
+    # print(np.random.choice([10,20,30,4,5,6]))
+    # print("Distribute:")
+    # print(np.random.beta(1,10,100))
+
+    #3 Array Opes
+    # lst=np.arange(1,11).reshape([2,5])
+    # print("Exp")
+    # print(np.exp(lst))
+    # print("Exp2")
+    # print(np.exp2(lst))
+    # print("Sqrt")
+    # print(np.sqrt(lst))
+    # print("Sin")
+    # print(np.sin(lst))
+    # print("Log")
+    # print(np.log(lst))
+
+    # lst=np.array([[[1,2,3,4],
+    #                 [4,5,6,7]],
+    #                 [[7,8,9,10],
+    #                 [10,11,12,13]],
+    #                 [[14,15,16,17],
+    #                 [18,19,20,21]]
+    #                 ])
+    # print("Sum")
+    # print(lst.sum())
+    # print("----")
+    # print(lst.sum(axis=0))
+    # print("----")
+    # print(lst.sum(axis=1))
+    # print("----")
+    # print(lst.sum(axis=2))
+    # print("Max")
+    # print(lst.max())
+    # print("----")
+    # print(lst.max(axis=0))
+    # print("----")
+    # print(lst.max(axis=1))
+    # print("----")
+    # print(lst.max(axis=2))
+    # print("Min")
+    # print(lst.min())
+    # print("----")
+    # print(lst.min(axis=0))
+    # print("----")
+    # print(lst.min(axis=1))
+    # print("----")
+    # print(lst.min(axis=2))
+
+    # lst1 = np.array([10,20,30,40])
+    # lst2 = np.array([4,3,2,1])
+    # print("Add")
+    # print(lst1+lst2)
+    # print("Sub")
+    # print(lst1-lst2)
+    # print("Mul")
+    # print(lst1*lst2)
+    # print("Div")
+    # print(lst1/lst2)
+    # print("Square")
+    # print(lst1**2)
+    # print("Dot")
+    # print(np.dot(lst1.reshape([2,2]),lst2.reshape([2,2])))
+    # print("Cancatenate")
+    # print(np.concatenate((lst1,lst2),axis=0))
+    # print(np.vstack((lst1,lst2)))
+    # print(np.hstack((lst1,lst2)))
+    # print(np.split(lst1,2))
+    # print(np.copy(lst1))
+
+    #4 liner
+    # print(np.eye(3))
+    # lst = np.array([[1,2],
+    #                 [3,4]])
+    # print("Inv:") #矩阵的逆
+    # print(inv(lst))
+    # print("T:") #转置矩阵
+    # print(lst.transpose())
+    # print("Det:") #矩阵列式
+    # print(det(lst))
+    # print("Eig:")
+    # print(eig(lst))
+    # y=np.array([[5],[7]])
+    # print("Solve")
+    # print(solve(lst,y))
+
+    
 
 if __name__=="__main__":
     main()
